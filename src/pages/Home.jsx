3745c5b7b513b3f8c5ebdf63a3ecdb73dc99ff18
Ranjith101 <89287908+Ranjith101@users.noreply.github.com>
2023-06-23 11:38:09 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,7 @@ import AddTask from "../components/AddTask";
 const Home = () => {
   const [isOpen, setİsOpen] = useState(false);
   const [text, setText] = useState("Show Task Bar");
-  const [task, setTask] = useState([]); // axios ile gelen veriyi task içerisine atalım.
-  // https://mockapi.io/projects'den eğitim amaçlı api oluşturduk.
+  const [task, setTask] = useState([]); 
   const url = "https://63522c2b9d64d7c713102872.mockapi.io/api/task";
 
   const toggle = () => {
@@ -17,15 +16,12 @@ const Home = () => {
     setText(buttonText);
   };
 
-  //CRUD  -READ-
-
   const getTask = async () => {
     const { data } = await axios(url);
     setTask(data);
     console.log(data);
   };
 
-  // getTask(); useEffect olmadan yalın getTask()'i çağırdığımızda render-state (infinitive loop) durumu oluşur. Engellemek için useEffect didmount kullanırız. Böylece ilk renderda getTask() çalışır.Sonra birdaha çalışmaz.
 
   useEffect(() => {
     getTask();
